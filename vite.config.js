4c4594bad3ac9aaf581b4f7ef883707f9341414d
Redin00 <182083659+Redin00@users.noreply.github.com>
2026-06-19 23:41:04 +0200

    Restoring NPM settings.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import topLevelAwait from "vite-plugin-top-level-await";
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    port: 10000,        // the port to run on
-    host: '0.0.0.0',   // bind to all network interfaces (accessible from other devices)
-    strictPort: true,  // optional: fail if port is already in use instead of trying the next one
-  }
-})
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
